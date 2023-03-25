464ff9122c33b4f121f3dead9a9df13d101b665a
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-03-25 21:54:26 +0900

    feat : item2

--- pages/screens/screen2/index.js
+++ pages/screens/screen2/index.js
@@ -1,6 +1,6 @@
 import { useCallback } from "react";
 import { Text, TouchableHighlight, View } from "react-native";
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
   FlatList,
   SafeAreaView,
@@ -26,9 +26,9 @@ const ItemData = [
   },
 ];
 
-const Item = ({item, onPress, backgroundColor, textColor}) => (
-  <TouchableOpacity onPress={onPress} style={[styles.item, {backgroundColor}]}>
-    <Text style={[styles.title, {color: textColor}]}>{item.title}</Text>
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, { backgroundColor }]}>
+    <Text style={[styles.title, { color: textColor }]}>{item.title}</Text>
   </TouchableOpacity>
 );
 
@@ -36,64 +36,64 @@ function DetailsScreen({ navigation }) {
 
   const [selectedId, setSelectedId] = useState();
 
-    const onClick = useCallback(() => {
-      navigation.navigate("Home");
-    }, [navigation]);
-  
+  const onClick = useCallback(() => {
+    navigation.navigate("Home");
+  }, [navigation]);
 
-    const renderItem = ({item}) => {
-      const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
-      const color = item.id === selectedId ? 'white' : 'black';
-  
-      return (
-        <Item
-          item={item}
-          onPress={() => setSelectedId(item.id)}
-          backgroundColor={backgroundColor}
-          textColor={color}
-        />
-      );
-    };
-  
+
+  const renderItem = ({ item }) => {
+    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#f9c2ff';
+    const color = item.id === selectedId ? 'white' : 'black';
+
     return (
-      <SafeAreaView>
-        <FlatList
-         style={styles.container}
-          data={ItemData}
-          renderItem={renderItem}
-          keyExtractor={item => item.id}
-          extraData={selectedId}
-        />
-      </SafeAreaView>
+      <Item
+        item={item}
+        onPress={() => setSelectedId(item.id)}
+        backgroundColor={backgroundColor}
+        textColor={color}
+      />
     );
-    // return (
-    //   <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-    //     <TouchableHighlight onPress={onClick}>
-    //       <Text>디테일 스크린</Text>
-    //     </TouchableHighlight>
-    //   </View>
-    // );
-  }
-  
+  };
 
-  const styles = StyleSheet.create({
-    container: {
-      display:'flex',
-      marginTop: StatusBar.currentHeight || 0,
-      backgroundColor:'blue',
-      flexDirection:'row',
-      margin:'auto'
-    },
-    item: {
-      padding: 20,
-      marginVertical: 8,
-      marginHorizontal: 16,
-      width:150,
-      flexDirection: 'row'
-    },
-    title: {
-      fontSize: 32,
-    },
-  });
+  return (
+    <SafeAreaView>
+      <FlatList
+        style={styles.container}
+        data={ItemData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        extraData={selectedId}
+      />
+    </SafeAreaView>
+  );
+  // return (
+  //   <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+  //     <TouchableHighlight onPress={onClick}>
+  //       <Text>디테일 스크린</Text>
+  //     </TouchableHighlight>
+  //   </View>
+  // );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: 'blue',
+    flexDirection: 'row',
+    margin: 'auto'
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    width: 150,
+    flexDirection: 'row'
+  },
+  title: {
+    fontSize: 32,
+  },
+});
 
 export default DetailsScreen
