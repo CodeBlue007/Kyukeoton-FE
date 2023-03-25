4a6816eb247ad0eaba7a55b5184b0589ef0c4dbc
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-03-26 01:42:44 +0900

    feat : api 코드 추가

---
+++ api/API.js
@@ -0,0 +1,56 @@
+import axios from 'axios';
+
+const instance = axios.create();
+
+axios.defaults.baseURL = 'http://13.124.233.9:8080';
+instance.defaults.headers['Content-Type'] = 'application/json';
+
+
+const get = async (url) => {
+  try {
+    const { data } = await instance.get(url);
+    return data;
+  } catch (error) {
+    if (error instanceof Error)
+      throw new Error(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${error.message}`);
+  }
+};
+
+const del = async (url) => {
+  try {
+    const { data } = await instance.delete(url);
+    return data;
+  } catch (error) {
+    if (error instanceof Error)
+      throw new Error(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${error.message}`);
+  }
+};
+
+/**
+ *
+ * @param url : url주소 ex) /api/shops
+ * @param post : body
+ * @param config : default = null, 이미지 보낼때만 "imgPost" 설정 > 'Content-Type': 'multipart/form-data' 설정
+ * @returns : res.data 반환
+ */
+const post = async (url, post: {}) => {
+  try {
+    const result = await instance.post(url, post);
+    return result.data;
+  } catch (error) {
+    if (error instanceof Error)
+      throw new Error(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${error.message}`);
+  }
+};
+
+const patch = async (url, patch: {}) => {
+  try {
+    const { data } = await instance.patch(url, patch);
+    return data;
+  } catch (error) {
+    if (error instanceof Error)
+      throw new Error(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${error.message}`);
+  }
+};
+
+export { get, del as delete, post, patch };
