4134b1ff4af302ca58f64a1958ad4d4d35265035
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-03-25 21:53:57 +0900

    feat : button navigation추가

--- pages/screens/screen1/index.js
+++ pages/screens/screen1/index.js
@@ -1,30 +1,30 @@
 import { useCallback } from "react";
-import {Text, TouchableHighlight, View } from "react-native";
+import { Text, TouchableHighlight, View } from "react-native";
 import Button from "./components/Button";
 
 function HomeScreen({ navigation }) {
-    const onClick = useCallback(() => {
-      navigation.navigate("Details");
-    }, [navigation]);
+  const onClick = useCallback(() => {
+    navigation.navigate("Details");
+  }, [navigation]);
 
-    const a =1
-    const b = 2
-    const c =3
-    
-    
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" ,backgroundColor:'red'}}>
-        <TouchableHighlight onPress={onClick}>
-          <Text>홈 스크린</Text>
-        </TouchableHighlight>
-        <Button num={a}/>
-        <Button num={b}/>
-        <Button num={c}/>
-      </View>
-    );
-  }
+  const a = 1
+  const b = 2
+  const c = 3
 
-  export default HomeScreen;
+
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: 'red' }}>
+      <TouchableHighlight onPress={onClick}>
+        <Text>홈 스크린</Text>
+      </TouchableHighlight>
+      <Button num={a} navigation={navigation} />
+      <Button num={b} navigation={navigation} />
+      <Button num={c} navigation={navigation} />
+    </View>
+  );
+}
+
+export default HomeScreen;
 
 
 
