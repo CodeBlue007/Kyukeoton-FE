e7cfdb5986394c58a5fce4bb32ec41b77c0e79e3
hoyyChoi <hoyeon8165@gmail.com>
2023-03-26 00:29:56 +0900

    [add] 4ButtonGame-1 90%

--- pages/navigation/index.js
+++ pages/navigation/index.js
@@ -3,6 +3,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 import HomeScreen from "../screens/screen1"
 import DetailsScreen from "../screens/screen2"
+import DetailsScreen1 from "../screens/screen2/index1"
+import DetailsScreen2 from "../screens/screen2/index2"
+import DetailsScreen3 from "../screens/screen2/index3"
+import LastScreen from "../screens/screen2/index4"
 import SecondScreen from "../screens/screen3/SeondScreen"
 
 const Stack = createNativeStackNavigator()
@@ -13,18 +17,39 @@ const Navigation = () => {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: "메인", headerStyle: { backgroundColor: 'red' }, headerTintColor: 'blue', headerTitleStyle: { fontSize: '30px' } }}
+          options={{ title: "메인", headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' }}}
         />
 
         <Stack.Screen
           name="Details"
           component={DetailsScreen}
-          options={{ title: "디테일", headerBackVisible: false }}
+          options={{ title: "디테일",  headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' } ,headerBackVisible: false }}
+        />
+
+        <Stack.Screen
+          name="Details1"
+          component={DetailsScreen1}
+          options={{ title: "디테일",  headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' }, headerBackVisible: false }}
+        />
+        <Stack.Screen
+          name="Details2"
+          component={DetailsScreen2}
+          options={{ title: "디테일",  headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' },headerBackVisible: false }}
+        />
+         <Stack.Screen
+          name="Details3"
+          component={DetailsScreen3}
+          options={{ title: "디테일",  headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' },headerBackVisible: false }}
+        />
+        <Stack.Screen
+          name="Last"
+          component={LastScreen}
+          options={{ title: "Last",  headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' },headerBackVisible: false }}
         />
         <Stack.Screen
           name="SecondScreen"
           component={SecondScreen}
-          options={{ title: "디테일", headerBackVisible: false }}
+          options={{ title: "디테일",  headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' },headerBackVisible: false }}
         />
 
 
--- pages/screens/screen2/index2.js
+++ pages/screens/screen2/index2.js
@@ -6,13 +6,12 @@ import {LinearGradient} from "expo-linear-gradient"
 
 
 
-
-function DetailsScreen({ navigation }) {
+function DetailsScreen2({ navigation }) {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [correct,setCorrect] = useState(false);
 
     const onClick = useCallback(() => {
-      navigation.navigate("Details1");
+      navigation.navigate("Details3");
     }, [navigation]);
 
     const close = ()=>{
@@ -27,12 +26,12 @@ function DetailsScreen({ navigation }) {
 
 
     return (
-      <LinearGradient
+        <LinearGradient
         colors={['#0091EA','#9EDAFF']}
         style={styles.container1}
         >
         <View style={{ position:'relative',flex: 1, alignItems: "center"}}>
-            <Text style={styles.title1}>‘?’에 적합한 그림이 무엇일까요?</Text>
+            <Text style={styles.title1}>‘33333333333333</Text>
             <Text style={styles.title2}>계절</Text>
           <View style={styles.selectImg}>
             <View  style={{ flexDirection:'row'}}>
@@ -112,127 +111,126 @@ function DetailsScreen({ navigation }) {
  }
 
  const styles = StyleSheet.create({
-  container1:{
-    flex:1,
-    justifyContent:'center'
+    container1:{
+      flex:1,
+      justifyContent:'center'
+    },
+    selectImg:{
+      display:'flex',
+      marginTop:170,
+      borderRadius: 20,
+      overflow: "hidden"
+    },
+    btn:{
+      display:'flex',
+  
+      marginTop:33
+    },
+  
+    title1:{
+      marginTop:50,
+      width:310,
+      position:'absolute',
+      zIndex:1,
+      fontSize: 24,
+      lineHeight: 29,
+      fontWeight: 700,
+      color:'#ffffff'
+  
+    },
+    title2:{
+      marginTop:90,
+      width:310,
+      position:'absolute',
+      zIndex:1,
+      fontWeight: 700,
+      fontSize: 24,
+      lineHeight: 29,
+      textAlign:'center',
+      color:'#ffffff'
+    },
+  
+    container: {
+      width:147,
+      height:147,
+      backgroundColor:'#ffffff',
+      borderStyle:'solid',
+      borderColor:'white',
+      borderWidth:1.5
+    },
+  
+    centeredView: {
+    flex: 1,
+    position:"absolute",
+    bottom:30,
+  
   },
-  selectImg:{
+  modalView: {
+  
+    backgroundColor: 'white',
+    width:400,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    backgroundColor: "#ffffff",
+    padding: 18,
+    margin: 10,
+    borderRadius: 16,
+    width:165,
+    height:62,
     display:'flex',
-    marginTop:170,
-    borderRadius: 20,
-    overflow: "hidden"
+    justifyContent:'center',
+  
   },
-  btn:{
+  button1: {
+    backgroundColor: "#4B778D",
+    padding: 18,
+    marginTop: 20,
+    borderRadius: 8,
+    width:300,
     display:'flex',
-
-    marginTop:33
+    justifyContent:'center',
+  
   },
-
-  title1:{
-    marginTop:50,
-    width:310,
-    position:'absolute',
-    zIndex:1,
-    fontSize: 24,
-    lineHeight: 29,
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  button1Close: {
+    backgroundColor: 'red',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+    fontSize:25
+  },
+  modalText1: {
+    marginBottom: 15,
+    textAlign: 'center',
+    fontSize:20
+  },
+  buttonText:{
     fontWeight: 700,
-    color:'#ffffff'
+    lineHeight: 22,
+    fontSize: 18, 
+    color: "#0061C1",
+    textAlign:'center'
+  }
+  
+  });
+  
 
-  },
-  title2:{
-    marginTop:90,
-    width:310,
-    position:'absolute',
-    zIndex:1,
-    fontWeight: 700,
-    fontSize: 24,
-    lineHeight: 29,
-    textAlign:'center',
-    color:'#ffffff'
-  },
-
-  container: {
-    width:147,
-    height:147,
-    backgroundColor:'#ffffff',
-    borderStyle:'solid',
-    borderColor:'white',
-    borderWidth:1.5
-  },
-
-  centeredView: {
-  flex: 1,
-  position:"absolute",
-  bottom:30,
-
-},
-modalView: {
-
-  backgroundColor: 'white',
-  width:400,
-  padding: 35,
-  alignItems: 'center',
-  shadowColor: '#000',
-  shadowOffset: {
-    width: 0,
-    height: 2,
-  },
-  shadowOpacity: 0.25,
-  shadowRadius: 4,
-  elevation: 5,
-},
-button: {
-  backgroundColor: "#ffffff",
-  padding: 18,
-  margin: 10,
-  borderRadius: 16,
-  width:165,
-  height:62,
-  display:'flex',
-  justifyContent:'center',
-
-},
-button1: {
-  backgroundColor: "#4B778D",
-  padding: 18,
-  marginTop: 20,
-  borderRadius: 8,
-  width:300,
-  display:'flex',
-  justifyContent:'center',
-
-},
-buttonClose: {
-  backgroundColor: '#2196F3',
-},
-button1Close: {
-  backgroundColor: 'red',
-},
-textStyle: {
-  color: 'white',
-  fontWeight: 'bold',
-  textAlign: 'center',
-},
-modalText: {
-  marginBottom: 15,
-  textAlign: 'center',
-  fontSize:25
-},
-modalText1: {
-  marginBottom: 15,
-  textAlign: 'center',
-  fontSize:20
-},
-buttonText:{
-  fontWeight: 700,
-  lineHeight: 22,
-  fontSize: 18, 
-  color: "#0061C1",
-  textAlign:'center'
-}
-
-});
-
-
-
-export default DetailsScreen
+export default DetailsScreen2
