14b5706230dc7747f686c2ee41274f325a72392b
Subin Lee <81899298+CodeBlue007@users.noreply.github.com>
2023-03-26 06:57:34 +0900

    feat : 네비게이션 추가

--- pages/components/Modall.js
+++ pages/components/Modall.js
@@ -1,8 +1,8 @@
 import { useCallback, useEffect } from "react";
-import React, {useState} from 'react';
-import { Text, TouchableHighlight, Modal,StyleSheet,View,Pressable,TouchableOpacity,Image} from "react-native";
+import React, { useState } from 'react';
+import { Text, TouchableHighlight, Modal, StyleSheet, View, Pressable, TouchableOpacity, Image } from "react-native";
 
-const Modall = ({isModalVisible,setIsModalVisible,correct,next,navigation,solution}) => {
+const Modall = ({ isModalVisible, setIsModalVisible, correct, next, navigation, solution }) => {
 
 
 
@@ -10,142 +10,142 @@ const Modall = ({isModalVisible,setIsModalVisible,correct,next,navigation,soluti
     navigation.navigate(next);
   }, [navigation]);
 
-  const close = ()=>{
+  const close = () => {
     setIsModalVisible(false)
   }
 
 
   return (
     <Modal
-    animationType={"slide"}
-    style={{width:40,height:10}}
-    transparent={true}
-    visible={isModalVisible}
-    onRequestClose={() => {
+      animationType={"slide"}
+      style={{ width: 40, height: 10 }}
+      transparent={true}
+      visible={isModalVisible}
+      onRequestClose={() => {
         isModalVisible(!isModalVisible)
-    }}
+      }}
     >
-    <View style={styles.centeredView}>
-    {correct?
-        <View style={styles.centeredView3}>
-        <Image
-        style={styles.tinyLogo}
-        source={require('../../assets/Subtract.png')}
-        />
-        </View>:
-        <View style={styles.centeredView2}>
-        <Image
-        style={styles.tinyLogo}
-        source={require('../../assets/Union.png')}
-        />
-        </View>}
-              
+      <View style={styles.centeredView}>
+        {correct ?
+          <View style={styles.centeredView3}>
+            <Image
+              style={styles.tinyLogo}
+              source={require('../../assets/Subtract.png')}
+            />
+          </View> :
+          <View style={styles.centeredView2}>
+            <Image
+              style={styles.tinyLogo}
+              source={require('../../assets/Union.png')}
+            />
+          </View>}
+
         <View style={styles.modalView}>
-        {correct?
-        <View style={{display:'flex',flexDirection:'row'}}>
-          <Image
-          style={styles.tinyLogo2}
-          source={require('../../assets/yes1.png')}
-          />
-          <Text style={styles.modalText2}> 참 잘했어요</Text>
-        </View>
-        :
-        <View style={{display:'flex',flexDirection:'row'}}>
-          <Image
-          style={styles.tinyLogo1}
-          source={require('../../assets/no.png')}
-          />
-          <Text style={styles.modalText}> 오답이에요</Text>
-        </View>
-        }
-              <Text style={styles.modalText1}>{correct?'':`정답 : ${solution}`}</Text>
-              <Pressable
-                style={correct?[styles.button1, styles.buttonClose]:[styles.button1, styles.button1Close]}
-                onPress={()=>{
-                  close()
-                  onClick()
-                }}>
-                <Text style={styles.textStyle}>{next==="Last"?'확인':'계속하기'}</Text>
-              </Pressable>
+          {correct ?
+            <View style={{ display: 'flex', flexDirection: 'row' }}>
+              <Image
+                style={styles.tinyLogo2}
+                source={require('../../assets/yes1.png')}
+              />
+              <Text style={styles.modalText2}> 참 잘했어요</Text>
             </View>
-          </View>
+            :
+            <View style={{ display: 'flex', flexDirection: 'row' }}>
+              <Image
+                style={styles.tinyLogo1}
+                source={require('../../assets/no.png')}
+              />
+              <Text style={styles.modalText}> 오답이에요</Text>
+            </View>
+          }
+          <Text style={styles.modalText1}>{correct ? '' : `정답 : ${solution}`}</Text>
+          <Pressable
+            style={correct ? [styles.button1, styles.buttonClose] : [styles.button1, styles.button1Close]}
+            onPress={() => {
+              close()
+              onClick()
+            }}>
+            <Text style={styles.textStyle}>{next === "Last" ? '확인' : '계속하기'}</Text>
+          </Pressable>
+        </View>
+      </View>
     </Modal>
   )
 }
 
 const styles = StyleSheet.create({
-    container1:{
-      flex:1,
-      justifyContent:'center'
-    },
-    selectImg:{
-      display:'flex',
-      marginTop:170,
-      borderRadius: 20,
-      overflow: "hidden"
-    },
-    btn:{
-      display:'flex',
-  
-      marginTop:33
-    },
-    tinyLogo1:{
-      width:30,
-      height:30,
-      zIndex:5
-    },
-    tinyLogo2:{
-      width:32,
-      height:32,
-      zIndex:6
-    },
-  
-    title1:{
-      marginTop:50,
-      width:310,
-      position:'absolute',
-      zIndex:1,
-      fontSize: 24,
-      lineHeight: 29,
-      fontWeight: 700,
-      color:'#ffffff'
-  
-    },
-    title2:{
-      marginTop:90,
-      width:310,
-      position:'absolute',
-      zIndex:1,
-      fontWeight: 700,
-      fontSize: 24,
-      lineHeight: 29,
-      textAlign:'center',
-      color:'#ffffff'
-    },
-  
-    container: {
-      width:147,
-      height:147,
-      backgroundColor:'#ffffff',
-      borderStyle:'solid',
-      borderColor:'white',
-      borderWidth:1.5
-    },
-  
-    centeredView: {
+  container1: {
     flex: 1,
-    position:"absolute",
-    bottom:0,
-    
-  
+    justifyContent: 'center'
+  },
+  selectImg: {
+    display: 'flex',
+    marginTop: 170,
+    borderRadius: 20,
+    overflow: "hidden"
+  },
+  btn: {
+    display: 'flex',
+
+    marginTop: 33
+  },
+  tinyLogo1: {
+    width: 30,
+    height: 30,
+    zIndex: 5
+  },
+  tinyLogo2: {
+    width: 32,
+    height: 32,
+    zIndex: 6
+  },
+
+  title1: {
+    marginTop: 50,
+    width: 310,
+    position: 'absolute',
+    zIndex: 1,
+    fontSize: 24,
+    lineHeight: 29,
+    fontWeight: 700,
+    color: '#ffffff'
+
+  },
+  title2: {
+    marginTop: 90,
+    width: 310,
+    position: 'absolute',
+    zIndex: 1,
+    fontWeight: 700,
+    fontSize: 24,
+    lineHeight: 29,
+    textAlign: 'center',
+    color: '#ffffff'
+  },
+
+  container: {
+    width: 147,
+    height: 147,
+    backgroundColor: '#ffffff',
+    borderStyle: 'solid',
+    borderColor: 'white',
+    borderWidth: 1.5
+  },
+
+  centeredView: {
+    flex: 1,
+    position: "absolute",
+    bottom: 0,
+
+
   },
   modalView: {
-  
+
     backgroundColor: 'white',
-    position:'relative',
-    width:390,
+    position: 'relative',
+    width: 390,
     padding: 30,
-    borderRadius:20,
+    borderRadius: 20,
     alignItems: 'center',
     shadowColor: '#000',
     shadowOffset: {
@@ -157,26 +157,26 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   centeredView2: {
-    flex:1,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    position:"relative",
-    bottom:100
-   
-  
+    position: "relative",
+    bottom: 100
+
+
   },
   centeredView3: {
-    flex:1,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    position:"relative",
-    bottom:73
-   
-  
+    position: "relative",
+    bottom: 73
+
+
   },
   modalView2: {
-  backgroundColor:'#ffffff',
-    width:400,
+    backgroundColor: '#ffffff',
+    width: 400,
     padding: 35,
     alignItems: 'center',
     shadowColor: '#000',
@@ -193,11 +193,11 @@ const styles = StyleSheet.create({
     padding: 18,
     margin: 10,
     borderRadius: 16,
-    width:165,
-    height:62,
-    display:'flex',
-    justifyContent:'center',
-  
+    width: 165,
+    height: 62,
+    display: 'flex',
+    justifyContent: 'center',
+
   },
   button1: {
     backgroundColor: "#4B778D",
@@ -205,11 +205,11 @@ const styles = StyleSheet.create({
     marginTop: 18,
     marginBottom: 50,
     borderRadius: 8,
-    width:350,
-    height:62,
-    display:'flex',
-    justifyContent:'center',
-  
+    width: 350,
+    height: 62,
+    display: 'flex',
+    justifyContent: 'center',
+
   },
   buttonClose: {
     backgroundColor: '#2196F3',
@@ -220,25 +220,25 @@ const styles = StyleSheet.create({
   textStyle: {
     color: '#ffffff',
     textAlign: 'center',
-      fontStyle: 'normal',
+    fontStyle: 'normal',
     fontWeight: 700,
     fontSize: 18,
-   lineHeight: 22
+    lineHeight: 22
   },
   modalText: {
     marginBottom: 15,
     textAlign: 'center',
-    fontSize:21,
+    fontSize: 21,
     lineHeight: 25,
-    fontWeight:700,
+    fontWeight: 700,
     color: '#DB3434'
   },
   modalText2: {
     marginBottom: 15,
     textAlign: 'center',
-    fontSize:21,
+    fontSize: 21,
     lineHeight: 25,
-    fontWeight:700,
+    fontWeight: 700,
     color: '#3498DB'
   },
   modalText1: {
@@ -248,15 +248,15 @@ const styles = StyleSheet.create({
     lineHeight: 25,
     color: '#3C3C3C'
   },
-  buttonText:{
+  buttonText: {
     fontWeight: 700,
     lineHeight: 22,
-    fontSize: 18, 
+    fontSize: 18,
     color: "#0061C1",
-    textAlign:'center'
+    textAlign: 'center'
   }
-  
-  });
+
+});
 
 
-export default Modall
+export default Modall;
--- pages/navigation/index.js
+++ pages/navigation/index.js
@@ -7,8 +7,16 @@ import DetailsScreen1 from "../screens/screen2/index1"
 import DetailsScreen2 from "../screens/screen2/index2"
 import DetailsScreen3 from "../screens/screen2/index3"
 import LastScreen from "../screens/screen2/index4"
-import SecondScreen from "../screens/screen3/SeondScreen"
-import Screen3 from "../screens/screen4/Screen3"
+import SecondScreen1 from "../screens/screen3/SeondScreen1"
+import SecondScreen2 from "../screens/screen3/SeondScreen2"
+import SecondScreen3 from "../screens/screen3/SeondScreen3"
+import SecondScreen4 from "../screens/screen3/SeondScreen4"
+import ThirdScreen1 from "../screens/screen4/ThirdScreen1"
+import ThirdScreen2 from "../screens/screen4/ThirdScreen2"
+import ThirdScreen3 from "../screens/screen4/ThirdScreen3"
+import ThirdScreen4 from "../screens/screen4/ThirdScreen4"
+
+
 
 const Stack = createNativeStackNavigator()
 const Navigation = () => {
@@ -24,23 +32,23 @@ const Navigation = () => {
         <Stack.Screen
           name="Details"
           component={DetailsScreen}
-          options={{ title: "디테일", headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' }, headerBackVisible: false }}
+          options={{ title: "기본인지능력", headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' }, headerBackVisible: false }}
         />
 
         <Stack.Screen
           name="Details1"
           component={DetailsScreen1}
-          options={{ title: "디테일", headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' }, headerBackVisible: false }}
+          options={{ title: "기본인지능력", headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' }, headerBackVisible: false }}
         />
         <Stack.Screen
           name="Details2"
           component={DetailsScreen2}
-          options={{ title: "디테일", headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' }, headerBackVisible: false }}
+          options={{ title: "기본인지능력", headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' }, headerBackVisible: false }}
         />
         <Stack.Screen
           name="Details3"
           component={DetailsScreen3}
-          options={{ title: "디테일", headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' }, headerBackVisible: false }}
+          options={{ title: "기본인지능력", headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' }, headerBackVisible: false }}
         />
         <Stack.Screen
           name="Last"
@@ -48,15 +56,48 @@ const Navigation = () => {
           options={{ title: "Last", headerStyle: { backgroundColor: '#0091EA' }, headerTintColor: '#ffffff', headerTitleStyle: { fontSize: '30px' }, headerBackVisible: false }}
         />
         <Stack.Screen
-          name="SecondScreen"
-          component={SecondScreen}
-          options={{ title: "스크린2", headerBackVisible: false }}
+          name="SecondScreen1"
+          component={SecondScreen1}
+          options={{ title: "시각적 주의력", headerBackVisible: false }}
         />
         <Stack.Screen
-          name="ThirdScreen"
-          component={Screen3}
-          options={{ title: "스크린3", headerBackVisible: false }}
+          name="SecondScreen2"
+          component={SecondScreen2}
+          options={{ title: "시각적 주의력", headerBackVisible: false }}
         />
+        <Stack.Screen
+          name="SecondScreen3"
+          component={SecondScreen3}
+          options={{ title: "시각적 주의력", headerBackVisible: false }}
+        />
+        <Stack.Screen
+          name="SecondScreen4"
+          component={SecondScreen4}
+          options={{ title: "시각적 주의력", headerBackVisible: false }}
+        />
+        <Stack.Screen
+          name="ThirdScreen1"
+          component={ThirdScreen1}
+          options={{ title: "수학적 사고력", headerBackVisible: false }}
+        />
+        <Stack.Screen
+          name="ThirdScreen2"
+          component={ThirdScreen2}
+          options={{ title: "수학적 사고력", headerBackVisible: false }}
+        />
+
+        <Stack.Screen
+          name="ThirdScreen3"
+          component={ThirdScreen3}
+          options={{ title: "수학적 사고력", headerBackVisible: false }}
+        />
+
+        <Stack.Screen
+          name="ThirdScreen4"
+          component={ThirdScreen4}
+          options={{ title: "수학적 사고력", headerBackVisible: false }}
+        />
+
 
 
       </Stack.Navigator>
